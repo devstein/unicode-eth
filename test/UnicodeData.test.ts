82e32349f68ee27a2b01175c9c21084a536a24ee
Devin Stein <devstein@seas.upenn.edu>
2022-01-09 17:33:59 -0800

    WIP tests

---
+++ test/UnicodeData.test.ts
@@ -0,0 +1,65 @@
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { expect } from "chai";
+
+describe("UnicodeData Contract", function () {
+  it("should deploy", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should initialize", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should return a character from a string", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should return a character from a codepoint", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should allow the owner to set a character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should not allow non-owners to set a character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the numeric type for a character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the numeric value for a character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify uppercase characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify lowercase characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify titlecase characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify cased characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify alphabetic characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify math characters", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should identify emoji characters 🤓", async function () {
+    expect(false).to.equal(true);
+  });
+});
---
+++ test/UnicodeLibrary.test.ts
@@ -0,0 +1,53 @@
+import { ethers } from "hardhat";
+import { Signer } from "ethers";
+import { expect } from "chai";
+
+describe("Unicode Library", function () {
+  it("should decode from position 0", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should fail to decode on an invalid position", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should decode a single character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should decode a part of a string", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should decodeAll of a string", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should decodeAll of a single character", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the codePointAt at an index", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the codePointAt at the last index", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should failed to get the codePointAt at an out of bounds index", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the charAt at an index", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should get the charAt at the last index", async function () {
+    expect(false).to.equal(true);
+  });
+
+  it("should failed to get the charAt at an out of bounds index", async function () {
+    expect(false).to.equal(true);
+  });
+});
