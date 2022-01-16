c3df3394a77e3fadf237bf39662ca6254dce45a4
Devin Stein <devstein@seas.upenn.edu>
2022-01-16 13:51:11 -0800

    Add UTF8Encoder contract

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -2,31 +2,21 @@ import { ethers } from "hardhat";
 import { Contract } from "ethers";
 
 const UNICODE_LIBRARY = "Unicode";
+const UTF8_ENCODER_LIBRARY = "UTF8Encoder";
 const UNICODE_DATA_CONTRACT = "UnicodeData";
 
-export const deployUnicodeData = async (): Promise<Contract> => {
-  const contractFactory = await ethers.getContractFactory(
-    UNICODE_DATA_CONTRACT
-  );
+export const deploy = async (id: string): Promise<Contract> => {
+  const contractFactory = await ethers.getContractFactory(id);
   const contract = await contractFactory.deploy();
   await contract.deployed();
-  console.log(
-    `${UNICODE_DATA_CONTRACT} contract deployed: ${contract.address}`
-  );
-  return contract;
-};
-
-export const deployUnicodeLibrary = async (): Promise<Contract> => {
-  const contractFactory = await ethers.getContractFactory(UNICODE_LIBRARY);
-  const contract = await contractFactory.deploy();
-  await contract.deployed();
-  console.log(`${UNICODE_LIBRARY} contract deployed: ${contract.address}`);
+  console.log(`${id} contract deployed: ${contract.address}`);
   return contract;
 };
 
 export const deployAll = async (): Promise<Contract[]> => {
-  const unicodeData = await deployUnicodeData();
-  const unicodeLibrary = await deployUnicodeLibrary();
+  const unicodeData = await deploy(UNICODE_DATA_CONTRACT);
+  const unicodeLibrary = await deploy(UNICODE_LIBRARY);
+  const utf8Encoder = await deploy(UTF8_ENCODER_LIBRARY);
 
-  return [unicodeData, unicodeLibrary];
+  return [unicodeData, unicodeLibrary, utf8Encoder];
 };
